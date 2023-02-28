bf8c26b466f35b694e4c668672ef07c2f2372e8f
Cullen Ruscin <cullen.ruscin@icloud.com>
2023-02-28 10:25:00 -0600

    add boilerplate html elements

--- scripts/main.js
+++ scripts/main.js
@@ -42,3 +42,20 @@ Bookings.js
     3.  Available bands beside the venues
 
 */
+
+const mainContainer = document.querySelector('#container')
+
+const applicationHTML = `
+<h1>Tour Some Sugar On Me</h1>
+<article class="bookings">
+    <h2>Bookings</h2>
+</article>
+<article class="venues">
+    <h2>Venues</h2>
+</article>
+<article class="bands">
+    <h2>Bands</h2>
+</article>
+`
+
+mainContainer.innerHTML = applicationHTML
